9eae558cec35f59f2ac8fff9d0598b6614bd48a3
muzakkirhossainminhaz <minhaz.bauet@gmail.com>
2022-07-07 16:00:53 +0600

    slider updated

--- src/apifolder/SliderAPI.js
+++ src/apifolder/SliderAPI.js
@@ -2,15 +2,29 @@ export const SliderAPI = [
     {
         id: 0,
         src: 'https://i.ibb.co/rkBVsJ1/slider01.jpg',
-
+        content: {
+            h2: 'Amazing Offer in Eid-ul-Azha',
+            p: 'Up to 40% off',
+        },
+        background: 'bg-[#fbb64f]',
     },
     {
         id: 1,
         src: 'https://i.ibb.co/jkj4sFk/slider02.jpg',
+        content: {
+            h2: 'Amazing Offer in Eid-ul-Fitr',
+            p: 'Up to 50% off',
+        },
+        background: 'bg-[#000000]',
     },
     {
         id: 2,
         src : 'https://i.ibb.co/jfRZ0R3/slider03.jpg',
+        content: {
+            h2: 'Amazing Offer in New Year',
+            p: 'Up to 30% off',
+        },
+        background: 'bg-[#fff7ea]',
     },
 ];
 
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRounded';
 import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
 import slidesApi from '../apifolder/SliderAPI';
+import './Slider.css';
 
 const Slider = () => {
 
     const [slides] = useState(slidesApi);
-    console.log(slides);
+    // console.log(slides);
 
     const [activeSlide, setActiveSlide] = useState(0);
 
@@ -37,10 +38,17 @@ const Slider = () => {
                     console.log(slide);
                     if (idx === activeSlide) {
                         return (
-                            <div className="current-slide flex justify-center items-center w-[100%] h-[500px] shadow-2xl rounded-xl border-[#c0c0c0] border-10px overflow-hidden relative">
+                            <div className={`current-slide flex justify-center items-center h-[500px] shadow-2xl rounded-xl border-[#c0c0c0] border-10px overflow-hidden relative ${slide.background}`}>
                                 <div className="slide flex justify-center items-center h-[100%]">
-                                    <div className='img w-[100%] h-[100%]'>
-                                        <img className='w-[100%] h-[100%] object-contain' src={slide.src} alt='first slider' />
+                                    <div className='img flex-1 h-[100%]'>
+                                        <img className='h-[100%] object-contain' src={slide.src} alt='first slider' />
+                                    </div>
+                                    <div className="discription flex flex-col flex-1 place-items-start justify-center ml-11">
+                                        <h2 className="text-white text-5xl font-bold">{slide.content.h2}</h2>
+                                        <p className=" text-4xl font-semibold text-white">{slide.content.p}</p>
+                                        <button className="btn">
+                                            Shop Now
+                                        </button>
                                     </div>
                                 </div>
                             </div>
@@ -50,7 +58,7 @@ const Slider = () => {
             }
 
             <div className="right-arrow rounded-full bg-grey flex justify-center items-center shadow-sm hover:cursor-pointer p-[6px]">
-                <ArrowForwardIosRoundedIcon style={{ fontSize: '50px' }} onClick={nextSlide}/>
+                <ArrowForwardIosRoundedIcon style={{ fontSize: '50px' }} onClick={nextSlide} />
             </div>
         </div>
     );
